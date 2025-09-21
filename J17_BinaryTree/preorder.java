1c8750129237e9fbb9357eebfc03f16c49dd8f13
Ashaf Khan <khanashaf02@gmail.com>
2025-09-21 13:33:27 +0530

    Added BinaryTree folder

---
+++ J17_BinaryTree/preorder.java
@@ -0,0 +1,24 @@
+package J17_BinaryTree;
+
+public class preorder {
+
+    class TreeNode{
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int val){
+            this.val = val;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    
+
+    public static void main(String[] args){
+        int[] arr = {1, 2, -1, 4, 5, 6};
+
+        
+
+    }
+}
