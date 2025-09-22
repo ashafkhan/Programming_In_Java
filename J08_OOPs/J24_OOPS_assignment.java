109e82ae29f9a021aef782628e576803a0996e20
Ashaf Khan <khanashaf02@gmail.com>
2025-09-22 23:51:36 +0530

    modified OOPS_assignment file

--- J08_OOPs/J24_OOPS_assignment.java
+++ J08_OOPs/J24_OOPS_assignment.java
@@ -1,13 +1,14 @@
 package J08_OOPs;
-public class J24_OOPS_assignment{
-    public static void main(String args[]){
-
-    }
-}
 
 class Complex{
     int real, imag;
     void addComplex(int a, int b){
         
+    }
+}
+
+public class J24_OOPS_assignment{
+    public static void main(String args[]){
+
     }
 }
